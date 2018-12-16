d581ba4abe2a3f5c3dcf18e66e5f8fb3acf75cd8
Rishat <xpan96@gmail.com>
2018-12-16 21:13:24 +0300

    array

--- js/main.js
+++ js/main.js
@@ -7,28 +7,13 @@ Vue.filter ('capatilize', function(value) {
 new Vue({
     el: '#app',
     data: {
-       show: true,
        cars: [
            {model: "BMW", speed: 250.0},
            {model: "Audi", speed: 240.5},
            {model: "Ford", speed: 160.0},
-           {model: "Mercedes", speed: 250.0}
-       ],
-       message: "hey, rishat7c" 
-    },
-    computed: {
-        showMess() {
-            return this.message.toUpperCase()
-        }
-    },
-    filters: {
-        lowercase(value) {
-            return value.toLowerCase()
-        }
-    },
-    methods: {
-        changeText () {
-            this.title = 'Our text...';    
-        }
+           {model: "Mercedes", speed: 250.0},
+           {model: "KIA", speed: 200},
+           {model: "VAZ", speed: 160}
+       ]
     }
 });
