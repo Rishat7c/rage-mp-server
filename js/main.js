d86e0f9ae987e33f3acbfdf674a331be6828de6e
Rishat <xpan96@gmail.com>
2018-12-16 21:26:33 +0300

    created component: template & logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-Vue.filter ('capatilize', function(value) {
-    if(!value) return ''
-    value = value.toString()
-    return value.replace(/\b\w/g, function(l) { return l.toUpperCase() })
-});
+Vue.component('app-car', {
+    data: function() {
+        return {
+            cars: [
+                {model: "BMW", speed: 250.0},
+                {model: "Audi", speed: 240.5},
+                {model: "Ford", speed: 160.0},
+                {model: "Mercedes", speed: 250.0},
+                {model: "KIA", speed: 200},
+                {model: "VAZ", speed: 160}
+            ]
+        }
+    },
+    template: '<div><div class="car" v-for="car in cars"><p> {{ car.model }}, speed {{ car.speed }} </p></div></div>'
+})
 
 new Vue({
     el: '#app',
     data: {
-       cars: [
-           {model: "BMW", speed: 250.0},
-           {model: "Audi", speed: 240.5},
-           {model: "Ford", speed: 160.0},
-           {model: "Mercedes", speed: 250.0},
-           {model: "KIA", speed: 200},
-           {model: "VAZ", speed: 160}
-       ]
+       
     }
 });
